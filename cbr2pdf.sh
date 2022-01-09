4d575072356e1af295b122997c35b31e363a1db1
Angela Carraro <angela_carraro@hotmail.it>
2022-01-09 21:51:29 +0100

    Modified variable name

--- cbr2pdf.sh
+++ cbr2pdf.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 
+green=$'\e[32m'
+red=$'\e[31m'
+yellow=$'\e[33m'
+blue=$'\e[34m'
+reset=$'\e[0m'
+
 SECONDS=0 # bash built in "timer"
 
 # NOTE : Quote it else use array to avoid problems #
@@ -11,22 +17,24 @@ DIRR="$(basename "${FILES}")"
 OUTDIRR="Output/"
 FILES2="./Output/*"
 
-for f in $FILES
+for file in $FILES
 do
-  # take action on each file. $f store current file name
-  echo -e "\n\nProcessing $f file..."
+  # take action on each file. $file store current file name
+  echo -e "${blue}Processing $file file...${reset}"
 
   # Extract files from each archive
-  name=$(basename "${f}")  # part of the POSIX standard
+  name=$(basename "${file}")  # part of the POSIX standard
   name=$(echo $name | cut -d')' -f 1)')'
   echo -e "\toutput name=${name}"
-  7z e "${f}" -o"./${OUTDIRR}${name}"
+  7z e "${file}" -o"./${OUTDIRR}${name}"
 
   # Merge the .jpgs in only one pdf
   img2pdf -o "${OUTDIRR}${name}".pdf ./"${OUTDIRR}${name}"/*.jpg
 
   # Delete extracted files
   rm -r "./${OUTDIRR}${name}"
+
+  echo -e "\n\n"
 done
 
 echo -e "\nTotal time: ${SECONDS}s"
