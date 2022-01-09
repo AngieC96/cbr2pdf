7c73543315d334b784e3e9ad704bde455d3d307a
Angela Carraro <angela_carraro@hotmail.it>
2022-01-09 15:57:45 +0100

    Added time computation

--- script.sh
+++ script.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+
+SECONDS=0 # bash built in "timer"
+
 # NOTE : Quote it else use array to avoid problems #
 FILES="/home/angela/Documenti/Topolini/Topolini2/*"
 
 DIRR="Output/"
 FILES2="./Output/*"
 
+# Extract files from each archive
 for f in $FILES
 do
   # take action on each file. $f store current file name
@@ -18,9 +22,12 @@ done
 
 echo -e "\n"
 
+# Merge the .jpgs in only one pdf
 for f in $FILES2
 do
   echo -e "Processing $f file..."
   name=$(echo $f |cut -d'/' -f 3)
   img2pdf -o "${DIRR}${name}".pdf ./"${DIRR}${name}"/*.jpg
 done
+
+echo -e "\nTotal time: ${SECONDS}s"
