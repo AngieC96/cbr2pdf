4519e22794088ef13648aaad50f13061d47a51f9
Angela Carraro <angela_carraro@hotmail.it>
2022-01-09 16:49:03 +0100

    Better input directory parsing

--- cbr2pdf.sh
+++ cbr2pdf.sh
@@ -6,7 +6,9 @@ SECONDS=0 # bash built in "timer"
 # NOTE : Quote it else use array to avoid problems #
 FILES="/home/angela/Documenti/Topolini/Topolini2/*"
 
-DIRR="Output/"
+DIRR="$(basename "${FILES}")"
+
+OUTDIRR="Output/"
 FILES2="./Output/*"
 
 # Extract files from each archive
@@ -14,10 +16,10 @@ for f in $FILES
 do
   # take action on each file. $f store current file name
   echo -e "\n\nExtracting $f file..."
-  name=$(echo $f |cut -d'/' -f 7)
+  name=$(basename "${f}")  # part of the POSIX standard
   name=$(echo $name | cut -d')' -f 1)')'
   echo -e "\toutput name=${name}"
-  7z e "${f}" -o"./${DIRR}${name}"
+  7z e "${f}" -o"./${OUTDIRR}${name}"
 done
 
 echo -e "\n"
@@ -27,7 +29,7 @@ for f in $FILES2
 do
   echo -e "Processing $f file..."
   name=$(echo $f |cut -d'/' -f 3)
-  img2pdf -o "${DIRR}${name}".pdf ./"${DIRR}${name}"/*.jpg
+  img2pdf -o "${OUTDIRR}${name}".pdf ./"${OUTDIRR}${name}"/*.jpg
 done
 
 echo -e "\nTotal time: ${SECONDS}s"
