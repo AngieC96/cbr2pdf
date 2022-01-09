639fffdc1cb5337fa8c234180ff9b7e67312672b
Angela Carraro <angela_carraro@hotmail.it>
2022-01-09 16:52:24 +0100

    Changed processing sequence Instead of extracting all archives and then creating all the PDFs, now it extracts one archive and then immediately creates the PDF.

--- cbr2pdf.sh
+++ cbr2pdf.sh
@@ -11,24 +11,18 @@ DIRR="$(basename "${FILES}")"
 OUTDIRR="Output/"
 FILES2="./Output/*"
 
-# Extract files from each archive
 for f in $FILES
 do
   # take action on each file. $f store current file name
-  echo -e "\n\nExtracting $f file..."
+  echo -e "\n\nProcessing $f file..."
+
+  # Extract files from each archive
   name=$(basename "${f}")  # part of the POSIX standard
   name=$(echo $name | cut -d')' -f 1)')'
   echo -e "\toutput name=${name}"
   7z e "${f}" -o"./${OUTDIRR}${name}"
-done
 
-echo -e "\n"
-
-# Merge the .jpgs in only one pdf
-for f in $FILES2
-do
-  echo -e "Processing $f file..."
-  name=$(echo $f |cut -d'/' -f 3)
+  # Merge the .jpgs in only one pdf
   img2pdf -o "${OUTDIRR}${name}".pdf ./"${OUTDIRR}${name}"/*.jpg
 done
 
